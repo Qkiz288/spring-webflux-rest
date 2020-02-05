276ddaccacbdf9588a29a22f1e2635f0421dd99c
Qkiz288 <kkukielka@griddynamics.com>
2020-02-05 16:22:45 +0100

    initial commit

---
+++ src/test/java/com/kkukielka/springwebfluxrest/SpringWebfluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkukielka.springwebfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
