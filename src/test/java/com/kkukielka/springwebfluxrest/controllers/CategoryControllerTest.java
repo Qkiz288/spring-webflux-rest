369375af15ff21d48af877fc398b6f141ae6a7ef
Qkiz288 <kkukielka@griddynamics.com>
2020-02-06 17:49:37 +0100

    [415] Implemented method for creating Category, added unit tests

--- src/test/java/com/kkukielka/springwebfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/com/kkukielka/springwebfluxrest/controllers/CategoryControllerTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
 class CategoryControllerTest {
@@ -51,4 +53,22 @@ class CategoryControllerTest {
                 .expectBody(Category.class);
 
     }
+
+    @Test
+    void createCategory() {
+        // given
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().build()));
+
+        Mono<Category> categoryToSave = Mono.just(Category.builder().description("test").build());
+
+        // when - then
+        webTestClient.post().uri(CategoryController.BASE_URL)
+                .body(categoryToSave, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+
+    }
+
 }
